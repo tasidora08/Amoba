5fdc5761f5a9638d9d0439cc40c1f98c48eeaa0d
tasidora08 <tasi.dora08@gmail.com>
2026-05-13 06:35:23 +0200

    .

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
 function Ertek(){
-    let kiir = document.getElementById("meret").value;
-    let hova = document.getElementById("szam");
-    hova.innerText = kiir;
+    let meret = document.getElementById("meret").value;
+    document.getElementById("szam").innerText = meret;
 }
 
 function Feltolt(){
+
     let meret = document.getElementById("meret").value;
-    let bele = document.getElementById("container");
-    bele.innerText = '';
-    for (i = 0; i < meret; i++){
-        let sor = document.createElement("div");
-        for (j = 0; j < meret; j++){
-            let kulon = document.createElement("div");
-            sor.appendChild(kulon);
+
+    let container = document.getElementById("container");
+
+    container.innerHTML = "";
+
+    container.style.gridTemplateColumns = `repeat(${meret}, 50px)`;
+
+    for(let i = 0; i < meret; i++){
+
+        for(let j = 0; j < meret; j++){
+
+            let div = document.createElement("div");
+
+            div.className = "mezo";
+
+            div.onclick = function(){
+
+                // ha már van benne valami
+                if(div.innerText != ""){
+                    return;
+                }
+
+                let valasztott =
+                document.querySelector('input[name="jel"]:checked').value;
+
+                div.innerText = valasztott;
+
+                Ellenoriz();
+            }
+
+            container.appendChild(div);
         }
-        bele.appendChild(sor);
     }
 }
 
-function Lerak(){
-    let radioV = document.getElementById("mi");
-    let div = document.querySelector("div");
-    if (radioV == "X") {
-        div.innerText = "X";
-    }
-    if (radioV == "O") {
-        div.innerText = "O";
+function Ellenoriz(){
+
+    let mezok = document.querySelectorAll(".mezo");
+
+    for(let mezo of mezok){
+
+        if(mezo.innerText != ""){
+
+            document.getElementById("uzenet").innerText =
+            "Van már lerakott jel!";
+        }
     }
 }
