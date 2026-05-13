6b4241d8a3ba391b1187305e269d7cabb0de3dd7
tasidora08 <tasi.dora08@gmail.com>
2026-05-13 08:42:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,51 +4,32 @@ function Ertek(){
 }
 
 function Feltolt(){
-
     let meret = document.getElementById("meret").value;
-
     let container = document.getElementById("container");
-
     container.innerHTML = "";
-
     container.style.gridTemplateColumns = `repeat(${meret}, 50px)`;
-
     for(let i = 0; i < meret; i++){
-
         for(let j = 0; j < meret; j++){
-
             let div = document.createElement("div");
-
             div.className = "mezo";
-
             div.onclick = function(){
-
-                // ha már van benne valami
                 if(div.innerText != ""){
                     return;
                 }
-
                 let valasztott =
                 document.querySelector('input[name="jel"]:checked').value;
-
                 div.innerText = valasztott;
-
                 Ellenoriz();
             }
-
             container.appendChild(div);
         }
     }
 }
 
 function Ellenoriz(){
-
     let mezok = document.querySelectorAll(".mezo");
-
     for(let mezo of mezok){
-
         if(mezo.innerText != ""){
-
             document.getElementById("uzenet").innerText =
             "Van már lerakott jel!";
         }
